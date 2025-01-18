5d2c7502b6b9557a60066551c0d78bae30a6599c
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-01-18 17:16:14 +0700

    UPDATE DECRIPTION ABOUT

--- src/pages/home/sections/About.jsx
+++ src/pages/home/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
                     I am a web developer who enjoys turning ideas into <em>functional</em> and <em>useful web applications</em>. My focus is on creating user experiences that are <em>simple, engaging, and easy to use</em>.
                 </p>
                 <p>
-                    I recently graduated with a Bachelor’s degree in Computer Science, majoring in <em>Informatics Engineering</em>, from a private university in Jakarta. Currently, I am interning at an NGO, <em>Madani Berkelanjutan</em>, as a <em>Full Stack Web Developer Intern</em>. In this role, I contribute to <em>redesigning the website</em> and <em>implementing new features</em> to enhance its functionality and user experience.
+                    I graduated with a Bachelor’s degree in Computer Science, majoring in Informatics Engineering, from a private university in Jakarta three months ago. I recently completed an internship at an NGO, Madani Berkelanjutan, where I worked as a Full Stack Web Developer Intern. During my internship, I contributed to redesigning the organization’s website and implementing new features to enhance its functionality and user experience.
                 </p>
                 <p>
                     I am constantly learning and evolving as a developer, exploring <em>new technologies</em> and <em>methodologies</em> to improve my skills. My goal is to build web solutions that not only meet functional requirements but also provide a <em>meaningful impact</em> on users. I look forward to collaborating on projects that <em>challenge me</em> and contribute to creating better digital experiences.
