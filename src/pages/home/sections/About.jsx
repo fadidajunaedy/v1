ba972a70d5549e2644ada2a36e251c0ca32bbec2
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-01-24 12:35:01 +0700

    UPDATE DESCRIPTION ABOUT ME

--- src/pages/home/sections/About.jsx
+++ src/pages/home/sections/About.jsx
@@ -4,13 +4,13 @@ const About = () => {
             <article id="about" className="flex flex-col gap-4">
                 <h2 className="sticky top-0 lg:hidden">ABOUT</h2>
                 <p>
-                    I am a web developer who enjoys turning ideas into <em>functional</em> and <em>useful web applications</em>. My focus is on creating user experiences that are <em>simple, engaging, and easy to use</em>.
+                    I love working with <em>HTML</em>, <em>CSS</em>, and <em>JavaScript</em>, but I'm happy to use the tools most suitable for the job, such as <em>React.JS</em>.
                 </p>
                 <p>
-                    I graduated with a Bachelor’s degree in Computer Science, majoring in Informatics Engineering, from a private university in Jakarta three months ago. I recently completed an internship at an NGO, Madani Berkelanjutan, where I worked as a Full Stack Web Developer Intern. During my internship, I contributed to redesigning the organization’s website and implementing new features to enhance its functionality and user experience.
+                    I hold a Bachelor’s degree in Computer Science, majoring in Informatics Engineering, from a private university in Jakarta. My experience includes contributing to web development projects, where I worked on redesigning websites and implementing features to enhance functionality and user experience.
                 </p>
                 <p>
-                    I am constantly learning and evolving as a developer, exploring <em>new technologies</em> and <em>methodologies</em> to improve my skills. My goal is to build web solutions that not only meet functional requirements but also provide a <em>meaningful impact</em> on users. I look forward to collaborating on projects that <em>challenge me</em> and contribute to creating better digital experiences.
+                    As a developer, I am constantly learning and improve my skills. I look forward to collaborating on projects that challenge me and contribute to creating better digital experiences.
                 </p>
             </article>
         </>
